624b093667fdf7ca629d57979f4dbb4ba5d6cf2f
Laicure <Laicure@outlook.com>
2017-05-20 04:32:25 +0800

    minor fixes, .000 -> .fff timings

--- PsyBeacon/Resources/Table Structure.sql
+++ PsyBeacon/Resources/Table Structure.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[PsyMain](
-	[IDx] [varchar](128) NOT NULL,
+	[IDx] [varchar](128) NOT NULL primary key,
 	[Username] [varchar](64) NOT NULL,
 	[Name] [nvarchar](192) NOT NULL,
 	[WinTitle] [nvarchar](max) NULL,
 	[WinProcess] [nvarchar](max) NULL,
-	[TimeLogged] [datetime] NOT NULL
+	[TimeLogged] [datetime2] NOT NULL
 )
